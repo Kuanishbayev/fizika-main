26b7af1ec20866f5ced4d38dbbb6ce4a625b9ec8
Ernazar <kuanishbayevyernazar@gmail.com>
2024-07-13 12:32:28 +0500

    Added document download function.

--- src/pages/Qosimshalar/test-sorawlari/sorawlar-teksti/Test_7_klass.tsx
+++ src/pages/Qosimshalar/test-sorawlari/sorawlar-teksti/Test_7_klass.tsx
@@ -1,6 +1,12 @@
+import { FaFileDownload } from "react-icons/fa";
+
 export const Test_7_klass = () => {
+
     return (
         <div className="pdf-box">
+            <a href="https://docs.google.com/document/d/1l424H34brlqDZ915jdXUD3dFrmOtfQaF/export?format=docx" title="Download this file" className="download-file">
+                <FaFileDownload />
+            </a>
             <iframe src="https://docs.google.com/document/d/1l424H34brlqDZ915jdXUD3dFrmOtfQaF/preview" className='docc' allow="autoplay"></iframe>
         </div>
     )
